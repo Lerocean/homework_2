c2f0fff16ff20740e49ecbb42f9732fbfca123ad
lerocean <vpodezva@gmail.com>
2024-02-06 11:30:03 +0300

    homework fruit

--- src/main/java/fruits/Apple.java
+++ src/main/java/fruits/Apple.java
@@ -5,8 +5,4 @@ public class Apple extends Fruit {
     Apple() {
         super(1.0f);
     }
-    @Override
-    public String toString(){
-        return "apples";
-    }
 }
--- src/main/java/fruits/Orange.java
+++ src/main/java/fruits/Orange.java
@@ -5,8 +5,4 @@ public class Orange extends Fruit {
     Orange() {
         super(1.5f);
     }
-    @Override
-    public String toString(){
-        return "apples";
-    }
 }
