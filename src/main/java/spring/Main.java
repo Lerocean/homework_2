26f89939940c19fb83f3970f538d8bbfd11276c7
lerocean <vpodezva@gmail.com>
2024-02-28 08:44:44 +0300

    homework spring context

--- src/main/java/spring/Main.java
+++ src/main/java/spring/Main.java
@@ -1,11 +1,10 @@
 package spring;
 
-import ru.spring.OrderDaoImpl;
-import ru.fedynko.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        FoodDeliveryService foodDeliveryService = context.getBean(FoodDeliveryService.class);
+        FoodDeliveryService foodDeliveryService = context.getBean(FoodDeliveryServiceImpl.class);
         String orderId = "123";
         foodDeliveryService.deliverFood(orderId);
     }
